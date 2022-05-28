d820596f6d8fb73958f753d5329a39b325e5d6c9
m-Cho1 <mturtle051@gmail.com>
2022-05-28 00:50:50 +0000

    progress string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,72 @@
 /* exported titleCase */
+
+function titleCase(title) {
+  var minorWords = ['In', 'For', 'Of', 'The', 'In', 'To', 'On', 'And'];
+  var toLow = title.toLowerCase();
+  var firstCap = toLow.charAt(0).toUpperCase() + toLow.substring(1);
+  var splitted = firstCap.split(' ');
+  // console.log('splitted: ', splitted);
+  var result = [];
+  for (var i = 0; i < splitted.length; i++) {
+    var word = splitted[i];
+    word = (word[0].toUpperCase() + word.substring(1));
+    if (minorWords.indexOf(word) !== -1) {
+      word = word.toLowerCase();
+    }
+    if (word === 'Api') {
+      word = 'API';
+    }
+    if (word === 'Javascript') {
+      word = 'JavaScript';
+    }
+    if (word === 'Javascript:') {
+      word = 'JavaScript:';
+    }
+    // console.log('word : ', word);
+
+    result.push(word);
+  }
+  var result1 = result[0].charAt(0).toUpperCase() + result[0].substring(1);
+  console.log('result : ', result);
+  return result.join(' ');
+}
+
+// - change input title into all lowercased, store into 'toLow' variable.
+// - capitalize the first word in 'toLow' and store into 'firstCap' variable.
+// - split 'firstCap' by single space, and store into 'splitted' variable.
+// - create an empty array 'result'.
+// - go through each element in the 'splitted' array.
+// - store the index of splitted array into 'word' variable.
+// - if word's length is greater than 3, uppercase the word's first character.
+// - if word's value is 'api, or 'Javascript', or 'Javascript:', give the value of 'JavaScript'.
+// - if the word's value is any 'minor' words, lowercase them.
+// - if the word is any 'major' words, uppercase the first character of word.
+// - if all conditions meet, push the word into the 'result' array.
+// - convert the 'result', and return as a return value.
+
+// function titleCase(title) {
+//   var minorWords = ['In', 'For', 'Of', 'The', 'In', 'To', 'On', 'And'];
+//   var toLow = title.toLowerCase();
+//   var firstCap = toLow.charAt(0).toUpperCase() + toLow.substring(1);
+//   var splitted = firstCap.split(' ');
+//   var result = [];
+//   for (var i = 0; i < splitted.length; i++) {
+//     var word = splitted[i];
+//     word = (word[0].toUpperCase() + word.substring(1));
+//     if (minorWords.indexOf(word) !== -1) {
+//       word = word.toLowerCase();
+//     }
+//     if (word === 'Api') {
+//       word = 'API';
+//     }
+//     if (word === 'Javascript') {
+//       word = 'JavaScript';
+//     }
+//     if (word === 'Javascript:') {
+//       word = 'JavaScript:';
+//     }
+//     console.log('word : ', word);
+//     result.push(word);
+//   }
+//   return result.join(' ');
+// }
