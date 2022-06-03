e03e9ab36d4eba0379381e9b3e87944f4c8a5715
m-Cho1 <mturtle051@gmail.com>
2022-06-03 00:40:15 +0000

    edited string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -56,31 +56,6 @@ function titleCase(title) {
 // - if the word's value is any 'minor' words, lowercase them.
 // - if the word is any 'major' words, uppercase the first character of word.
 // - if all conditions meet, push the word into the 'result' array.
+// - go through indexes in 'result', check if the index match with the word 'the', that needs to be uppercased.
+// - convert to 'The' if the index matches the condition.
 // - convert the 'result', and return as a return value.
-
-// function titleCase(title) {
-//   var minorWords = ['In', 'For', 'Of', 'The', 'In', 'To', 'On', 'And'];
-//   var toLow = title.toLowerCase();
-//   var firstCap = toLow.charAt(0).toUpperCase() + toLow.substring(1);
-//   var splitted = firstCap.split(' ');
-//   var result = [];
-//   for (var i = 0; i < splitted.length; i++) {
-//     var word = splitted[i];
-//     word = (word[0].toUpperCase() + word.substring(1));
-//     if (minorWords.indexOf(word) !== -1) {
-//       word = word.toLowerCase();
-//     }
-//     if (word === 'Api') {
-//       word = 'API';
-//     }
-//     if (word === 'Javascript') {
-//       word = 'JavaScript';
-//     }
-//     if (word === 'Javascript:') {
-//       word = 'JavaScript:';
-//     }
-//     console.log('word : ', word);
-//     result.push(word);
-//   }
-//   return result.join(' ');
-// }
