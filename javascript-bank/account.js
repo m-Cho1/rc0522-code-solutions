6b82685abc749dc6a27fccc32ec1b799a6dd9656
m-Cho1 <mturtle051@gmail.com>
2022-06-07 22:06:24 +0000

    finished account section

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -7,13 +7,36 @@ function Account(number, holder) {
 }
 
 Account.prototype.deposit = function (amount) {
-  var deposit = {
-    type: this.type,
-    amount: this.amount
-  };
-  if (deposit.amount < 0) {
-    return false;
-  } else {
+  if (Number.isInteger(amount) && amount > 0) {
+    var deposit = new Transaction('deposit', amount);
     this.transactions.push(deposit);
+    return true;
+  } else {
+    return false;
   }
+};
+
+Account.prototype.withdraw = function (amount) {
+  if (Number.isInteger(amount) && amount > 0) {
+    var withdraw = new Transaction('withdrawal', amount);
+    this.transactions.push(withdraw);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.getBalance = function () {
+  var deposits = 0;
+  var withdrawals = 0;
+  for (var i = 0; i < this.transactions.length; i++) {
+    if (this.transactions[i].type === 'deposit') {
+      deposits += this.transactions[i].amount;
+    } else if (this.transactions[i].type === 'withdrawal') {
+      withdrawals += this.transactions[i].amount;
+    }
+  }
+  var result = deposits - withdrawals;
+  return result;
+
 };
