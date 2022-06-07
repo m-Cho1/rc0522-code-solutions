9f15865b9ddd6c915b478a7f52dc14d774f96cb5
m-Cho1 <mturtle051@gmail.com>
2022-06-07 18:25:10 +0000

    completed javascript-constructors

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,23 @@
+var myObj = new Object();
+console.log('value of myObj: ', myObj);
+console.log('typeof myObj : ', typeof (myObj));
+
+var myArr = new Array();
+console.log('value of myArr : ', myArr);
+console.log('typeof myArr : ', typeof (myArr));
+
+var myFunc = new Function();
+console.log('value of myFunc : ', myFunc);
+console.log('typeof myFunc : ', typeof (myFunc));
+
+var myStr = new String();
+console.log('value of myStr: ', myStr);
+console.log('typeof myStr : ', typeof (myStr));
+
+var myNum = new Number();
+console.log('value of myNum : ', myNum);
+console.log('typeof myNum : ', typeof (myNum));
+
+var myBoolean = new Boolean();
+console.log('value of myBoolean : ', myBoolean);
+console.log('typeof myBoolean : ', typeof (myBoolean));
