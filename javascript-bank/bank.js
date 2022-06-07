852147ffd34a1e22114ccf717e361c2a91a22b91
m-Cho1 <mturtle051@gmail.com>
2022-06-07 22:54:45 +0000

    completed javascript-bank

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,39 @@
 /* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (Number.isInteger(balance) && balance > 0) {
+    var newAcct = new Account(this.nextAccountNumber, holder);
+    newAcct.deposit(balance);
+    this.accounts.push(newAcct);
+    this.nextAccountNumber++;
+    return newAcct.number;
+  } else {
+    return null;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    var currentCustomer = this.accounts[i];
+    if (currentCustomer.number === number) {
+      return currentCustomer;
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  if (this.accounts.length === 0) {
+    return 0;
+  }
+  var grandTotal = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    var currentCustomerBalance = this.accounts[i].getBalance();
+    grandTotal += currentCustomerBalance;
+  }
+  return grandTotal;
+};
