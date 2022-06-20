49e87e24e58a08ba7486c67cf1dbc0e56e27c358
m-Cho1 <mturtle051@gmail.com>
2022-06-20 22:44:02 +0000

    worked on creating functions for tie, highest point, and handling points

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -40,5 +40,46 @@ for (var i = 0; i < suits.length; i++) {
   }
 }
 
+// putting card scores:
+function handlePoints(cards) {
+  var total = 0;
+  for (var i = 0; i < cards.length; i++) {
+    if (cards[i].includes('A')) {
+      total += 11;
+    } else if (cards[i].includes('K') || cards[i].includes('Q') || cards[i].includes('J')) {
+      total += 10;
+    } else {
+      total += parseInt(cards[i]);
+    }
+  }
+  return total;
+}
+
+// finding highest points:
+function highestPoint(points) {
+  var highest = 0;
+  for (var i = 0; i < points.length; i++) {
+    if (points[i] > highest) {
+      highest = points[i];
+    }
+  }
+  return highest;
+}
+
+// for tie points:
+function tie(scores) {
+  var tieIdx = [];
+  for (var i = 0; i < scores.length; i++) {
+    for (var j = 0; j < scores.length; j++) {
+      if (i !== j) {
+        if (scores[i] === scores[j]) {
+          tieIdx.push(i);
+        }
+      }
+    }
+  }
+  return tieIdx;
+}
+
 // shuffle cards:
 var shuffleDeck = _.shuffle(deck);
