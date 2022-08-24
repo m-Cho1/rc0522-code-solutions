1a5f3195a586b2857d3c17fe2d9d98ae8a20173a
m-Cho1 <mturtle051@gmail.com>
2022-08-24 19:27:55 +0000

    completed react-carousel-component

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -12,37 +12,83 @@ class Carousel extends React.Component {
     this.intervalID = this.intervalID.bind(this);
   }
 
-  clickRight() {
-
+  clickRight(event) {
+    if (this.state.count === this.props.images.length - 1) {
+      this.setState({
+        count: 0
+      });
+    } else {
+      this.setState({
+        count: this.state.count + 1
+      });
+    }
+    clearInterval(this.timer);
+    this.intervalID();
   }
 
-  clickLeft() {
-
+  clickLeft(event) {
+    if (this.state.count === 0) {
+      this.setState({
+        count: this.props.images.length - 1
+      });
+    } else {
+      this.setState({
+        count: this.state.count - 1
+      });
+    }
+    clearInterval(this.timer);
+    this.intervalID();
   }
 
-  clickDots() {
-
+  clickDots(event) {
+    this.setState({
+      count: Number(event.target.id)
+    });
+    clearInterval(this.timer);
+    this.intervalID();
   }
 
   intervalID() {
+    this.timer = setInterval(() => {
+      if (this.state.count === this.props.images.length - 1) {
+        this.setState({ count: 0 });
+      } else {
+        this.setState({ count: this.state.count + 1 });
+      }
+    }, 3000);
+  }
 
+  componentDidMount() {
+    this.intervalID();
   }
 
   render() {
     return (
       <>
-        <div className='container'>
+        <div className='carousel'>
           <div className='row'>
             <div className='col-10'>
-              <i className="fas fa-chevron-left fa-3x"></i>
+              <i className="fas fa-chevron-left fa-3x" onClick={this.clickLeft}></i>
             </div>
             <div className='col-80'>
               <img src={`${this.props.images[this.state.count].imgLink}`} alt="" />
             </div>
             <div className='col-10'>
-              <i className="fas fa-chevron-right fa-3x"></i>
+              <i className="fas fa-chevron-right fa-3x" onClick={this.clickRight}></i>
             </div>
           </div>
+        </div>
+        <div className="dots-container">
+          {
+            this.props.images.map((name, index) => {
+              const dotsColor = this.state.count === index
+                ? 'fas fa-circle'
+                : 'far fa-circle';
+              return (
+                <i key={index} id={index} className={dotsColor} onClick={this.clickDots}></i>
+              );
+            })
+          }
         </div>
       </>
     );
